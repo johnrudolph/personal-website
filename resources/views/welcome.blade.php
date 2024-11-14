052b36503f80e359daba18e702330e345886d3b1
John Drexler <johnrudolphdrexler@gmail.com>
2024-11-14 11:31:38 -0500

    WIP

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,28 +8,104 @@
                     <p class="text-lg text-gray-600 mb-6">
                         Product Manager and Game Developer
                     </p>
+                    <div class="container mx-auto px-4 py-8 flex justify-center space-x-4">
+                        <!-- Bluesky -->
+                        <a href="https://bsky.app/profile/johnrudolphdrexler.com" target="_blank" class="text-gray-400 hover:text-gray-600">
+                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 600 600" aria-hidden="true">
+                                <path d="m135.72 44.03c66.496 49.921 138.02 151.14 164.28 205.46 26.262-54.316 97.782-155.54 164.28-205.46 47.98-36.021 125.72-63.892 125.72 24.795 0 17.712-10.155 148.79-16.111 170.07-20.703 73.984-96.144 92.854-163.25 81.433 117.3 19.964 147.14 86.092 82.697 152.22-122.39 125.59-175.91-31.511-189.63-71.766-2.514-7.3797-3.6904-10.832-3.7077-7.8964-0.0174-2.9357-1.1937 0.51669-3.7077 7.8964-13.714 40.255-67.233 197.36-189.63 71.766-64.444-66.128-34.605-132.26 82.697-152.22-67.108 11.421-142.55-7.4491-163.25-81.433-5.9562-21.282-16.111-152.36-16.111-170.07 0-88.687 77.742-60.816 125.72-24.795z"/>
+                            </svg>
+                        </a>
+
+                        <!-- LinkedIn -->
+                        <a href="https://www.linkedin.com/in/jdrexler/" target="_blank" class="text-gray-400 hover:text-gray-600">
+                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <path d="M20.447 20.452h-3.554v-5.569c0-1.328-.027-3.037-1.852-3.037-1.853 0-2.136 1.445-2.136 2.939v5.667H9.351V9h3.414v1.561h.046c.477-.9 1.637-1.85 3.37-1.85 3.601 0 4.267 2.37 4.267 5.455v6.286zM5.337 7.433c-1.144 0-2.063-.926-2.063-2.065 0-1.138.92-2.063 2.063-2.063 1.14 0 2.064.925 2.064 2.063 0 1.139-.925 2.065-2.064 2.065zm1.782 13.019H3.555V9h3.564v11.452zM22.225 0H1.771C.792 0 0 .774 0 1.729v20.542C0 23.227.792 24 1.771 24h20.451C23.2 24 24 23.227 24 22.271V1.729C24 .774 23.2 0 22.222 0h.003z"/>
+                            </svg>
+                        </a>
+
+                        <!-- Spotify -->
+                        <a href="https://open.spotify.com/user/1236651355?si=cebd2689a5034a48" target="_blank" class="text-gray-400 hover:text-gray-600">
+                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z"/>
+                            </svg>
+                        </a>
+
+                        <!-- Twitter -->
+                        <a href="https://twitter.com/johnrudolphdrex" target="_blank" class="text-gray-400 hover:text-gray-600">
+                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"/>
+                            </svg>
+                        </a>
+
+                        <!-- Instagram -->
+                        <a href="https://www.instagram.com/johnrudolphdrexler/" target="_blank" class="text-gray-400 hover:text-gray-600">
+                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                                <path d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"/>
+                            </svg>
+                        </a>
+
+                        <!-- Letterboxd -->
+                        <a href="https://letterboxd.com/johnrudolphdrex/" target="_blank" class="text-gray-400 hover:text-gray-600">
+                            <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 500 500" aria-hidden="true">
+                                <defs>
+                                    <rect id="path-1" x="0" y="0" width="129.847328" height="141.443299"></rect>
+                                    <rect id="path-3" x="0" y="0" width="129.847328" height="141.443299"></rect>
+                                </defs>
+                                <g id="letterboxd-decal-dots-pos-mono" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                    <circle id="Circle" fill="currentColor" cx="250" cy="250" r="250"></circle>
+                                    <g id="Dots" transform="translate(61.000000, 180.000000)">
+                                        <ellipse id="Green" fill="#FFFFFF" cx="189" cy="70" rx="70.0786517" ry="70"></ellipse>
+                                        <g id="Blue" transform="translate(248.152672, 0.000000)">
+                                            <mask id="mask-2" fill="white">
+                                                <use xlink:href="#path-1"></use>
+                                            </mask>
+                                            <g id="Mask"></g>
+                                            <ellipse fill="#FFFFFF" mask="url(#mask-2)" cx="59.7686766" cy="70" rx="70.0786517" ry="70"></ellipse>
+                                        </g>
+                                        <g id="Orange">
+                                            <mask id="mask-4" fill="white">
+                                                <use xlink:href="#path-3"></use>
+                                            </mask>
+                                            <g id="Mask"></g>
+                                            <ellipse fill="#FFFFFF" mask="url(#mask-4)" cx="70.0786517" cy="70" rx="70.0786517" ry="70"></ellipse>
+                                        </g>
+                                        <path d="M129.539326,107.063108 C122.810493,96.3149291 118.921348,83.611134 118.921348,70 C118.921348,56.388866 122.810493,43.6850709 129.539326,32.9368922 C136.268159,43.6850709 140.157303,56.388866 140.157303,70 C140.157303,83.611134 136.268159,96.3149291 129.539326,107.063108 L129.539326,107.063108 Z" id="Overlap" fill="currentColor"></path>
+                                        <path d="M248.460674,32.9368922 C255.189507,43.6850709 259.078652,56.388866 259.078652,70 C259.078652,83.611134 255.189507,96.3149291 248.460674,107.063108 C241.731841,96.3149291 237.842697,83.611134 237.842697,70 C237.842697,56.388866 241.731841,43.6850709 248.460674,32.9368922 L248.460674,32.9368922 Z" id="Overlap" fill="currentColor"></path>
+                                    </g>
+                                </g>
+                            </svg>
+                        </a>
+                    </div>
                 </div>
                 <div class="flex justify-center">
                     <img src="/images/headshot.png" alt="Profile Photo" class="rounded-lg shadow-lg w-64 h-64 object-cover">
                 </div>
             </div>
         </div>
-        <div>
-            
-        </div>
     </div>
 
     <!-- Work Section -->
-    <div class="bg-white py-16">
+    <div class="bg-white py-8">
         <div class="container mx-auto px-4 sm:px-6 lg:px-8">
             <h2 class="text-3xl font-bold text-gray-900 mb-8">Work</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
-                <!-- Add your work items here -->
+            <div class="grid grid-cols-1 gap-6">
+                <!-- Thunk -->
                 <div class="bg-gray-50 rounded-lg p-6">
-                    <h3 class="text-xl font-semibold mb-2">Project 1</h3>
-                    <p class="text-gray-600">Description of your work/project</p>
+                    <h3 class="text-xl font-semibold mb-2"><a href="https://thunk.dev" target="_blank">Thunk</a></h3>
+                    <p class="text-gray-600">I am a partner and product manager at Thunk. We build custom software for clients, and do consulting as well. If you want to work with us, reach out at john@thunk.dev.</p>
                 </div>
-                <!-- Add more work items as needed -->
+                
+                <!-- Binti -->
+                <div class="bg-gray-50 rounded-lg p-6">
+                    <h3 class="text-xl font-semibold mb-2"><a href="https://binti.com" target="_blank">Binti</a></h3>
+                    <p class="text-gray-600">Most recently I served as a Product Manager at Binti. We built a high-impact, mission driven product. I got my Product Management chops, and it made me want to get closer to the code.</p>
+                </div>
+                
+                <!-- Sovereign's Capital -->
+                <div class="bg-gray-50 rounded-lg p-6">
+                    <h3 class="text-xl font-semibold mb-2"><a href="https://sovereignscapital.com/" target="_blank">Sovereign's Capital</a></h3>
+                    <p class="text-gray-600">My career started with a long stint in Venture Capital. I spent thousands of hours talking to founders, analyzing companies, and talking strategy. It made me want to get closer to the product.</p>
+                </div>
             </div>
         </div>
     </div>
@@ -41,10 +117,13 @@
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6">
                 <!-- Add your games content here -->
                 <div class="bg-white rounded-lg p-6 shadow">
-                    <h3 class="text-xl font-semibold mb-2">Game Project</h3>
-                    <p class="text-gray-600">Description of your game or gaming interests</p>
+                    <h3 class="text-xl font-semibold mb-2"><a href="https://www.catacombian.com/" target="_blank">Colossi</a></h3>
+                    <p class="text-gray-600">In 2024 I published Colossi, a tactical card game that makes you feel clever and devious.</p>
                 </div>
-                <!-- Add more game items as needed -->
+                <div class="bg-white rounded-lg p-6 shadow">
+                    <h3 class="text-xl font-semibold mb-2"><a href="https://discord.gg/yfMGwBvTuj" target="_blank">Join the Discord</a></h3>
+                    <p class="text-gray-600">I have many more prototypes, and I'm always working on something. If you want to test them out, follow along, or hang out with other game designers, join the Discord.</p>
+                </div>
             </div>
         </div>
     </div>
@@ -52,14 +131,16 @@
     <!-- Other Section -->
     <div class="bg-white py-16">
         <div class="container mx-auto px-4 sm:px-6 lg:px-8">
-            <h2 class="text-3xl font-bold text-gray-900 mb-8">Other Interests</h2>
+            <h2 class="text-3xl font-bold text-gray-900 mb-8">Other stuff</h2>
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <!-- Add other interests/content here -->
                 <div class="bg-gray-50 rounded-lg p-6">
-                    <h3 class="text-xl font-semibold mb-2">Interest</h3>
-                    <p class="text-gray-600">Description of your other interests</p>
+                    <h3 class="text-xl font-semibold mb-2"><a href="https://lindseyevans.work/" target="_blank">Lindsey</a></h3>
+                    <p class="text-gray-600">I'm madly in love with Lindsey. She is my favorite person. She is also an incredibly talented art director.</p>
                 </div>
-                <!-- Add more items as needed -->
+                <div class="bg-gray-50 rounded-lg p-6">
+                    <h3 class="text-xl font-semibold mb-2"><a href="https://marcelogarciajj.com/nyc/" target="_blank">Jiu Jitsu</a></h3>
+                    <p class="text-gray-600">I'm a purple belt in Jiu Jitsu. If you train and you're in NYC, let's train together at Marcelo Garcia Academy.</p>
+                </div>
             </div>
         </div>
     </div>
